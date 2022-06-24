db7ad3cef2b27b9a585a6e9556c593b0e0101649
thakurankit6767 <thakurankt6767@gmail.com>
2022-06-24 20:17:54 +0530

    Done

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
 
-import Routes from './server/route.js';
-import Connection from './database/db.js';
+import Routes from "./server/route.js";
+import Connection from "./database/db.js";
 
 const app = express();
 
-
 dotenv.config();
 
-// To handle HTTP POST requests in Express.js version 4 and above, 
-// you need to install the middleware module called body-parser.
-// body-parser extracts the entire body portion of an incoming request stream and exposes it on req.body.
-app.use(bodyParser.json({extended: true }));
+app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/', Routes);
+app.use("/", Routes);
 
-// const USERNAME = process.env.DB_USERNAME;
-// const PASSWORD = process.env.DB_PASSWORD;
-const USERNAME=process.env.DB_CONNECT
-// const PORT = '8080';
+const USERNAME = process.env.DB_CONNECT;
+
 const PORT = process.env.PORT || 8080;
 Connection(USERNAME);
- 
-app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+
+app.listen(PORT, () =>
+  console.log(`Server is running successfully on PORT ${PORT}`)
+);
